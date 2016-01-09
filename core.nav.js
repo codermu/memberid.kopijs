a4928ab09e2dec13ba64e53e419f811ac0271d3a
Lukluk <luklukaha@gmail.com>
2016-01-10 00:32:35 +0700

    save history to  localStorage

--- core.nav.js
+++ core.nav.js
@@ -10,6 +10,7 @@ Kopi.prototype.navigate = function (to, noredirect) {
   var temp = []
   $('#message').html('')
   kopi.history.push(to)
+  localStorage.setItem('kopi.history',JSON.stringify(kopi.history))
   while( kopi.history[i] != to && i < kopi.history.length){
     console.log(kopi.history[i])
     temp.push(kopi.history[i])
